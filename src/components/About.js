66af4a4f1c5053f3dbc67192f79181a9b701b02b
katherineeli <katherineeli@users.noreply.github.com>
2020-05-18 00:22:57 -0400

    Update Content

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import profile from "../images/easter.jpg";
+import profile from "../images/profile.jpg";
+import oldWell from "../images/oldwell.jpg";
+import jmp from "../images/jmp.jpg";
+
 class About extends Component {
   render() {
     return (
       // <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli"></link>
-      <div className="content">
-        <div className="halfleft">
-          <img className= "profile" src={profile} alt="profile picture"/>
+      <div className="content" style={{maxWidth:1100}}>
+        <div id= "about" className="row center">
+          <div className="col half padding12">
+            <img id= "profile" src={profile} alt="Profile"/>
+          </div>
+          <div className="bodyemphasis" style={{paddingTop:50}}>
+            Hi! My name is Katherine Li.<br/><br/>
+            <div className="body">
+                I am a junior at the University of North Carolina at Chapel Hill studying Computer Science with minors in Information Science and Statistics and Analytics.
+              <div> <br/>
+                I am interested in web development, user experience and user interface design, and software engineering. 
+              </div> <br/>
+              <div>
+                I enjoy photography and design as personal projects and would like to continue to develop my skills. 
+              </div>
+              {/* add in icons and links */}
+            </div>
+          </div> 
         </div>
-        <div className="halfright bodyemphasis" style={{paddingTop:50}}>
-          Hi! My name is Katherine Li.
-            <div className="body" style={{paddingTop: 50}}>
-              I am a junior at the University of North Carolina at Chapel Hill studying Computer Science with minors in Information Science and Statistics and Analytics. 
-            <div style={{paddingTop: 25}}>
-              I am interested in web development, user experience and user interface design, and software engineering. 
+        <br/> <hr/>
+        <div id= "experience" className="row padding32">
+          <div className= "row subheading" style={{justifyContent:'left'}}>EXPERIENCE</div>
+          <div className="row center">
+            <div className="col padding12">
+              <img id="jmp" src={jmp} alt="JMP at SAS logo"/>
             </div>
-            <div style={{paddingTop: 25}}>
-              I enjoy photography and design as personal projects and would like to continue to develop my skills. 
-            </div>
+            <div className="col padding12"> 
+              <div className="bodyheader">Technical Intern - SAS JMP
+                <div className="body"><ul>
+                  <li></li>
+                  <li></li>
+                  <li></li></ul>	
+                </div>
+              </div>
             </div>
           </div>
+          <div className="row center">
+            <div className="col padding12">
+              <img id="oldwell" src={oldWell} alt="UNC Old Well"/>
+            </div>
+            <div className="col padding12"> 
+              <div className="bodyheader">Undergraduate Research Assistant - UNC School of Education
+                <div className="body"><ul>
+                  <li>Designed educational programs to be used in low-income middle schools for cliturally and linguistically diverse students</li>
+                  <li>Conducted tests on programs to discover and refine the activities presented prior to data collection</li>
+                  <li>Collected data from middle school classes to measure the proficiency of students and evaluate improvements for the programs</li></ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id= "projects"></div>
       </div>
     )
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,31 @@ import { Link } from 'react-router-dom';
 import '../styles/style.css';
 
 class NavBar extends Component {
+  componentDidMount() {
+    window.addEventListener("scroll", this.resizeHeaderOnScroll);
+  }
+  resizeHeaderOnScroll() {
+    const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      shrinkOn = 200,
+      headerEl = document.getElementById("navBar");
+
+    if (distanceY > shrinkOn) {
+      headerEl.classList.add("smaller");
+    } else {
+      headerEl.classList.remove("smaller");
+    }
+  }
+  // maybe add smooth scrolling
   render() {
     return (
-      <div className="navBar">
-        <div className="navTitle h1">
-          KATHERINE LI
-        </div>
-        <ul id="headerButtons">
-          <li className="navButton h3" style={{left: 720, width: 107}}><Link to="/About">ABOUT</Link></li>
-          <li className="navButton h3" style={{left: 847, width: 148}}><Link to="">PROJECTS</Link></li>
-          <li className="navButton h3" style={{left: 1015, width: 232}}><Link to="">PHOTOGRAPHY</Link></li>
-          <li className="navButton h3" style={{left: 1266, width: 112}}><Link to="">DESIGN</Link></li>
-        </ul>
+      <div id="navBar" className="navBar">
+        <a id="title" href="#about" className="navTitle h1">KATHERINE LI</a>
+        <nav>
+          <a id="button" href="#about" className="navButton h3" style={{left: 720, color:"black", textDecoration:"none"}}>ABOUT</a>
+          <a id="button" href="#projects" className="navButton h3" style={{left: 847, color:"black", textDecoration:"none"}}>PROJECTS</a>
+          <a id="button" href="" className="navButton h3" style={{left: 1015, color:"black", textDecoration:"none"}}>PHOTOGRAPHY</a>
+          <a id="button" href="" className="navButton h3" style={{left: 1266, color:"black", textDecoration:"none"}}>DESIGN</a>
+        </nav>
       </div>
     )
   }
